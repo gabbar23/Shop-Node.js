a5726c3bd1ae74f00e40293b1b7da978e6b2c24f
gabbar23 <amanjots399@gmail.com>
2022-10-29 00:50:22 +0530

    sql

--- dev/app.ts
+++ dev/app.ts
@@ -1,10 +1,13 @@
 import express from "express";
-
+ 
 import bodyParser from "body-parser";
 import path from "path";
 import rootDir from "./util/path";
 
 import { sequelize } from "./util/database";
+import { Product } from "./models/productModel";
+import { User } from "./models/user";
+
 import * as adminRoutes from "./routes/adminRoutes/adminProducts-route";
 import * as shopRoutes from "./routes/shop/shopProductList-route";
 import * as routes404 from "./routes/404";
@@ -20,8 +23,12 @@ app.use("/admin", adminRoutes.adminRoutes);
 app.use(shopRoutes.shopRoutes);
 
 app.use(routes404.router404);
+
+Product.belongsTo(User,{constraints:true,onDelete:'CASCADE'})
+User.hasMany(Product);
 sequelize
-  .sync()
+  .sync({force:true})
   .then()
   .catch((err) => console.log(err));
+
 app.listen(3000);
